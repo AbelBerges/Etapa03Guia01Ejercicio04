29b1b9c8e21e20d76e8a0d05b36bd1be2153ec4d
AbelBerges <abel.berges@gmail.com>
2023-08-16 05:22:26 -0300

    Subo el ejercicio 3 de la primer guía

---
+++ src/etapa03guia01ejercicio04/Categoria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package etapa03guia01ejercicio04;
+
+/**
+ *
+ * @author kalema
+ */
+public enum Categoria {
+    ELECTRONICA,
+    ROPA,
+    ALIMENTOS
+}
